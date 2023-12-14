a37f85b0edfe559aac2d370c88b95093165b37c5
亮荣齐 <zorongqi@gmail.com>
2023-12-14 13:34:44 +0800

    Update v2ray.sh
    取消解析判断

--- v2ray.sh
+++ v2ray.sh
@@ -296,23 +296,6 @@ getData() {
         DOMAIN=${DOMAIN,,}
         colorEcho ${BLUE}  " 伪装域名(host)：$DOMAIN"
 
-        if [[ -f ~/v2ray.pem && -f ~/v2ray.key ]]; then
-            colorEcho ${BLUE}  " 检测到自有证书，将使用其部署"
-            CERT_FILE="/etc/v2ray/${DOMAIN}.pem"
-            KEY_FILE="/etc/v2ray/${DOMAIN}.key"
-        else
-            resolve=`curl -sm8 ipget.net/?ip=${DOMAIN}`
-            if [ "$resolve" != "$v4" ] && [ "$resolve" != "$v6" ]; then
-		if echo $resolve | grep -q html; then
-  colorEcho ${BLUE}  " resolve ${resolve} , v4 ${v4} , v6 ${v6}"
-			colorEcho ${BLUE}  " 域名解析失败，请添加域名解析记录或等待DNS同步，稍后再试。"
-		else
-			colorEcho ${BLUE}  " ${DOMAIN} 解析结果：${resolve}"
-		fi
-                colorEcho ${RED}  " 域名未解析到当前服务器IP("${BLUE}"ipv4:"${RED}"${v4} / "${BLUE}"ipv6:"${RED}"${v6} )!"
-                exit 1
-            fi
-        fi
     fi
 
     echo ""
